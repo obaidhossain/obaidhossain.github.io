22a93fb1e1aedcc434d35afd208a1c7b5243da63
Obaid Hossain <obaidhossain.me@gmail.com>
2026-05-19 12:04:12 +0600

    Updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,35 @@
 export const projects = [
   {
     id: "01",
-    title: "Ajax Login/Signup",
+    title: "Placeholder Generator",
     description:
-      "Snippet for WordPress NoReload Login/Signup ",
-    tags: ["PHP", "JavaScript", "jQuery"],
-    liveUrl: "",
-    repoUrl: "https://github.com/obaidhossain/ajax-signup/tree/main",
-    status: "CODE",
-    featured: false,
+      "A simple Express.js server that generates placeholder images on the fly. Perfect for prototyping web designs, testing layouts, or anytime you need dummy images.",
+    tags: ["HTML", "JavaScript", "Node.js", "Express.js"],
+    liveUrl: "https://placeholder-generator.vercel.app/",
+    repoUrl: "https://github.com/obaidhossain/placeholder-generator/",
+    status: "LIVE",
+    featured: true,
   },
   {
     id: "02",
+    title: "Background Remover",
+    description:
+      "A simple next.js application that allows users to remove the background from images. It uses next.js and tailwindcss to provide an easy-to-use interface for uploading images and processing them to remove the background. Backend is powered by python, fastapi and removebg API.",
+    tags: ["Next.js", "Tailwind CSS", "Python", "FastAPI", "Removebg API"],
+    liveUrl: "https://bg-remover-ten-beryl.vercel.app/",
+    repoUrl: "https://github.com/obaidhossain/bg-remover/",
+    status: "LIVE",
+    featured: true,
+  },
+  {
+    id: "03",
     title: "Same Height jQuery",
     description:
       "Avoid leaving empty space of the divs. Using the jQuery snippet help you to equal height of all child divs",
     tags: ["JavaScript", "jQuery"],
     liveUrl: "",
     repoUrl: "https://github.com/obaidhossain/same-height-jquery/",
-    status: "CODE",
+    status: "SNIPPET",
     featured: false,
   },
 ];
